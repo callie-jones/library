2187aee679aa6111e2a5421f0261b875631bd60f
callie <callie.jones@colorado.edu>
2020-02-26 23:36:39 -0700

    fix avg compute bug

--- libraryController.py
+++ libraryController.py
@@ -33,12 +33,13 @@ def addAction(action, queue):
         # check if action does NOT exist in data add action otherwise update action
         if(not actObj['action'] in data.keys()):
             count = 1
-            data[actObj['action']] = {'avg': actObj['time'], 'count': count}
+            data[actObj['action']] = {'avg': actObj['time'], 'count': count, 'time': actObj['time']}
         else:
             count = data[actObj['action']]['count'] + 1
             # TODO handle explicit round
-            newAvg = (data[actObj['action']]['avg'] + actObj['time']) / count
-            data[actObj['action']] = {'avg': newAvg, 'count': count}
+            newTime = data[actObj['action']]['time'] + actObj['time']
+            newAvg = newTime / count
+            data[actObj['action']] = {'avg': newAvg, 'count': count, 'time': newTime}
         dataLock.release()
         queue.put(json.dumps({'status': 200, 'message': 'Success'}))
     except Exception as err:
